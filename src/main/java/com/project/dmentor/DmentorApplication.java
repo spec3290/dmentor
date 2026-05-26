7772ac03cbde2d2e927a17c00f334faf83df8d26
곽효오 <spec3290@gmail.com>
2026-05-26 17:28:01 +0900

    feat: 초기 프로젝트 생성

---
+++ src/main/java/com/project/dmentor/DmentorApplication.java
@@ -0,0 +1,13 @@
+package com.project.dmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dmentor/DmentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
